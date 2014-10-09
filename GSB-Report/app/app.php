0f2beb21154eab0080b613ca26f5ed1f3373a130
jprim <j.primaut@laposte.net>
2014-10-09 16:56:24 +0200

    Display login form

--- GSB-Report/app/app.php
+++ GSB-Report/app/app.php
@@ -8,10 +8,31 @@ ExceptionHandler::register();
 
 // Register service providers.
 $app->register(new Silex\Provider\DoctrineServiceProvider());
+$app->register(new Silex\Provider\SessionServiceProvider());
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
+
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
+/**Firewall**/
 
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+    'security.firewalls' => array(
+        'login' => array(
+            'pattern' => '^/login$',
+            'anonymous' => true
+        ),
+        'secured' => array(
+            'pattern' => '^.*$',
+            'logout' => true,
+            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
+            'users' => $app->share(function () use ($app) {
+                return new GSB\DAO\VisitorDAO($app['db']);
+            }),
+        ),
+    ),
+));
+            
 // Register services.
 $app['dao.family'] = $app->share(function ($app) {
     return new GSB\DAO\FamilyDAO($app['db']);
@@ -32,23 +53,7 @@ $app['dao.practitioner'] = $app->share(function ($app){
     return $practitionerDAO;
 }); 
 
-/**Firewall**/
-$app->register(new Silex\Provider\SecurityServiceProvider(), array(
-    'security.firewalls' => array(
-        'login' => array(
-            'pattern' => '^/login$',
-            'anonymous' => true
-        ),
-        'secured' => array(
-            'pattern' => '^.*$',
-            'logout' => true,
-            'form' => array('login_path' => '/login', 'check_path' => '/login_check'),
-            'users' => $app->share(function () use ($app) {
-                return new GSB\DAO\VisitorDAO($app['db']);
-            }),
-        ),
-    ),
-));
+
 
 $app['dao.visitor'] = $app->share(function ($app) {
     return new GSB\DAO\VisitorDAO($app['db']);
