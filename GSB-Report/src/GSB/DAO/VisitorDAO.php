691e2997dac804f94a1488cdc096189eb88d3176
jprim <j.primaut@laposte.net>
2014-10-09 16:26:02 +0200

    finishes of class VisitorDAO

--- GSB-Report/src/GSB/DAO/VisitorDAO.php
+++ GSB-Report/src/GSB/DAO/VisitorDAO.php
@@ -4,9 +4,66 @@ namespace GSB\DAO;
 
 use GSB\Domain\Visitor;
 
-class VisitorDAO extends DAO
+/** Security**/
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+
+
+class VisitorDAO extends DAO implements UserProviderInterface
 {
+    /**
+     * Returns a user matching the supplied id.
+     *
+     * @param integer $id
+     *
+     * @return GSB\Domain\Visitor|throws an exception if no matching user is found
+     */
+    public function find($id) {
+        $sql = "select * from visitor where visitor_id = ?";
+        $row = $this->getDb()->fetchAssoc($sql, array($id));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new \Exception("No user matching id " . $id);
+    }
     
+    /**
+     * {@inheritDoc}
+     */
+    public function loadUserByUsername($username)
+    {
+        $sql = "select * from visitor where user_name=?";
+        $row = $this->getDb()->fetchAssoc($sql, array($username));
+
+        if ($row)
+            return $this->buildDomainObject($row);
+        else
+            throw new UsernameNotFoundException(sprintf('User "%s" not found.', $username));
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function refreshUser(UserInterface $user)
+    {
+        $class = get_class($user);
+        if (!$this->supportsClass($class)) {
+            throw new UnsupportedUserException(sprintf('Instances of "%s" are not supported.', $class));
+        }
+        return $this->loadUserByUsername($user->getUsername());
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public function supportsClass($class)
+    {
+        return 'GSB\Domain\Visitor' === $class;
+    }
+
     
     /**
      * Creates a Visitor instance from a DB query result row.
